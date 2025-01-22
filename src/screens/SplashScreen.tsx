b8fea39257ebc0dd41f1ef2968db1d7c2d6368c0
davidmugisho <davidmugisho243@gmail.com>
2025-01-22 17:21:25 +0100

    first spalshScreen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,5 +1,3 @@
-// src/screens/SplashScreen.tsx
-
 import React from 'react';
 import { View, Text, Image, StatusBar, StyleSheet } from 'react-native';
 
@@ -9,27 +7,23 @@ export default function SplashScreen() {
       {/* Status Bar */}
       <StatusBar barStyle="light-content" backgroundColor="#020A3C" />
 
-      {/* Group 47 - Circular Graphics */}
-      <View style={styles.graphicsContainer}>
-        <View style={styles.largeCircle} />
-        <View style={styles.smallCircle} />
-      </View>
-
-      {/* Group 48 - Main Icon */}
-      <View style={styles.iconContainer}>
-        <View style={styles.iconWrapper}>
-          <Image
-            source={require('../../assets/images/2hands.jpg')}
-            style={styles.iconImage}
-            resizeMode="cover"
-          />
-        </View>
-      </View>
-
-      {/* App Name and Version */}
-      <View style={styles.footer}>
+      {/* App Name and Version at the Top */}
+      <View style={styles.header}>
         <Text style={styles.appName}>Bomoko Fund</Text>
         <Text style={styles.version}>Version 1.1.0</Text>
+      </View>
+
+      {/* Group 47 - Circular Graphics */}
+      <View style={styles.graphicsContainer}>
+        <View style={styles.largeCircle}>
+          <View style={styles.smallCircle}>
+            <Image
+              source={require('../../assets/images/2hands.jpg')}
+              style={styles.iconImage}
+              resizeMode="cover"
+            />
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -42,9 +36,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  graphicsContainer: {
+  header: {
     position: 'absolute',
-    top: 185,
+    bottom: 50,
+    alignItems: 'center',
+  },
+  appName: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: '800',
+  },
+  version: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '600',
+    marginTop: 5,
+  },
+  graphicsContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   largeCircle: {
     width: 442,
@@ -53,6 +64,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#B0B0B0',
     opacity: 0.5,
+    position: 'static',
+    top: 185,
+    left: -33,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   smallCircle: {
     width: 218,
@@ -60,41 +76,14 @@ const styles = StyleSheet.create({
     borderRadius: 109,
     borderWidth: 1,
     borderColor: '#B0B0B0',
-    position: 'absolute',
-    top: 28,
-    left: 28,
     opacity: 0.5,
-  },
-  iconContainer: {
-    position: 'absolute',
-    top: '39%',
-    left: '36%',
-  },
-  iconWrapper: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   iconImage: {
-    width: '100%',
-    height: '100%',
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 10,
-    width: '100%',
-    alignItems: 'center',
-  },
-  appName: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: '800',
-  },
-  version: {
-    color: 'white',
-    fontSize: 14,
-    fontWeight: '600',
-    marginTop: 8,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
+
